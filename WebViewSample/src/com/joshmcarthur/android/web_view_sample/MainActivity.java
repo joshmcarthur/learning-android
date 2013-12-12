1a8e6e86f88df3c81bb293f83445afe060678f28
Josh McArthur <joshua.mcarthur@gmail.com>
2013-12-12 22:12:14 +1300

    Add WebView sample

---
+++ WebViewSample/src/com/joshmcarthur/android/web_view_sample/MainActivity.java
@@ -0,0 +1,27 @@
+package com.joshmcarthur.android.web_view_sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+	WebView browser;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		browser = (WebView)findViewById(R.id.webkit);
+		browser.loadUrl("http://joshmcarthur.com");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
