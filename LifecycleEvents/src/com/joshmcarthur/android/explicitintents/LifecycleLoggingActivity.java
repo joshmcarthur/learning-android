cdf6d55cd846856c7b9cd5305e618880550e34c3
Josh McArthur <joshua.mcarthur@gmail.com>
2013-12-25 23:36:50 +1300

    Add LifecycleEvents demo application

---
+++ LifecycleEvents/src/com/joshmcarthur/android/explicitintents/LifecycleLoggingActivity.java
@@ -0,0 +1,61 @@
+package com.joshmcarthur.android.explicitintents;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class LifecycleLoggingActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		Log.d(getClass().getSimpleName(), "onCreate");
+		
+	}
+	
+	@Override
+	public void onRestart() {
+		super.onRestart();
+		Log.d(getClass().getSimpleName(), "onRestart");
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		Log.d(getClass().getSimpleName(), "onStart");
+	}
+	
+	@Override
+	public void onStop() {
+		super.onStop();
+		Log.d(getClass().getSimpleName(), "onStop");
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		Log.d(getClass().getSimpleName(), "onPause");
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.d(getClass().getSimpleName(), "onResume");
+	}
+	
+	@Override
+	public void onDestroy() {
+		Log.d(getClass().getSimpleName(), "onDestroy");
+		super.onDestroy();
+	}
+	
+	
+	public void showOther(View v) {
+		startActivity(new Intent(this, OtherActivity.class));
+	}
+
+}
---
+++ LifecycleEvents/src/com/joshmcarthur/android/explicitintents/OtherActivity.java
@@ -0,0 +1,15 @@
+package com.joshmcarthur.android.explicitintents;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OtherActivity extends Activity {
+
+	@Override 
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.other);
+	}
+
+
+}
